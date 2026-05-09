f7cce979d606b5763b5ee980e5fe72ef564d9860
chaitanya madavi <procoder7869@gmail.com>
2026-05-10 00:21:57 +0530

    frontend first push

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { CreateTodo } from './components/CreateTodo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <CreateTodo/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,17 @@
+export function CreateTodo(){
+    return <div>
+        <input style={{
+            margin: 10,
+            padding: 10,
+        }} type="text" placeholder="title"/> <br />
+        <input style={{
+            margin: 10,
+            padding: 10,
+        }}type="text" placeholder="description"/> <br />
+        
+        <button style={{
+            margin: 10,
+            padding: 10,
+        }}>Add a todo</button>
+    </div>
+}
